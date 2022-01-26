52e0e1b93b6696a9dd7e19fa2abd480c82af39ce
glaylson <glaylsonflf@gmail.com>
2022-01-25 22:39:10 -0300

    Ajuste helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+    def month_day_comma_year(datetime)
+        datetime.strftime('%B %e, %y')        
+    end 
+    
     def render_if(condition, template, record)
         render template, record if condition
     end
